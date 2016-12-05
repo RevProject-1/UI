4b2f87f4c9ff29e9dc07e3f0f5dfd28e3407c991
raykonopka <konopka.ray@gmail.com>
2016-12-05 10:04:50 -0500

    Add MVC project with identity. [#135473125]

---
+++ ClientManagement.UI/ClientManagement.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClientManagement.UI.MVC.Startup))]
+namespace ClientManagement.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
